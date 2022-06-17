b2ac028e06a0c1206ce6e868ce957759528bbe4a
dev coder <devcoderbuzz@gmail.com>
2022-06-18 01:48:50 +0800

    font-awsome working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook } from "@fortawesome/free-regular-svg-icons";
+
 function App() {
   return (
     <div className="App">
-      <p class="text-red-500">The quick brown fox...</p>
+      <p class="text-red-500">
+        The quick brown fox... <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faAddressBook}></FontAwesomeIcon>
+      </p>
     </div>
   );
 }
